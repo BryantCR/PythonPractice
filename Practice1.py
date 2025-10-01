8a07549e3682854ae8b64e37428422b79d04ca83
Bryan Cascante <89356007+BryantCR@users.noreply.github.com>
2025-10-01 08:59:40 -0600

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,41 @@
+
+
+
+num = 20 # let num = 20;
+flag = True # let flag = true;
+name = "Bryan" # let name = "Bryan";
+
+nums = [10,20,30,40,50,60] # let nums = [10,20,30,50,60];
+#Touple
+names = {"Bryan", "Alfredo", "Miguel", "Valeria"} 
+
+print( num )
+print( flag )
+print( name )
+print( nums )
+print( names )
+
+Car = {
+    "brand": "Toyota",
+    "model": "Corolla",
+    "year": 2020,
+    "color": "Red"
+}
+
+print( Car )
+
+whole = 100 # let whole = 100;
+decimal = 10.5 # let decimal = 10.5;
+text = "Hello, World!" # let text = "Hello, World!";   
+is_valid = False # let is_valid = false;
+items = [1, 2, 3, 4, 5] # let items = [1, 2, 3, 4, 5];
+
+print( type (whole) )
+print( type (decimal) )
+
+turntowhole = int(decimal) # let turntowhole = int(decimal);
+turntodecimal = float(whole) # let turntodecimal = float(whole);
+turntotext = str(whole) # let turntotext = str(whole);  
+
+print( turntowhole )
+print( turntodecimal )
